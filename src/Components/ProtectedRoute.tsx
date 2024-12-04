ae50ee5e17514c77e1f51d5c93bda8ee048f7362
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2024-12-04 16:01:45 +0200

    add Logout functionality

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
 import { ReactNode, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../lib/store";
+import { GlobalState } from "../lib/store";
 import { useNavigate } from "react-router-dom";
 
 export default function ProtectedRoute({children}:{children:ReactNode}) {
-  const {token} = useSelector((state:RootState)=>state.auth)
+  const {token} = useSelector((state:GlobalState)=>state.auth)
   const navigate = useNavigate();
     useEffect(()=>{
         if (!token) {
